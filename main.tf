3902aae51ab22a86ed42a1dd64abf7782ef5d6c2
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-03-14 13:57:55 +0000

    init

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "stg-portfolio-site-iam" {
+  name = "AWSCodePipelineServiceRole-us-east-1-portfolio-site"
+}
+
+resource "aws_s3" "stg-portfolio-site-s3-artifact" {
+
+}
+
+resource "aws_codepipeline" "stg-portfolio-site" {
+  name     = "stg.portfolio-site"
+  role_arn = "${aws_iam_role.stg-portfolio-site-iam.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.stg-portfolio-site-s3-artifact.bucket}"
+    type     = "S3"
+
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["test"]
+
+      configuration = {
+        Owner  = "my-organization"
+        Repo   = "test"
+        Branch = "master"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name            = "Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["test"]
+      version         = "1"
+
+      configuration = {
+        ProjectName = "test"
+      }
+    }
+  }
+}
